ff12aed9f7fe787815a2e88aa417feeff0c488fc
sonicj <sonicjhon56@gmail.com>
2025-11-25 15:09:58 +0700

    docs: Add docs for DatabaseTransactionOps and DatabaseTransactionIO

--- src/database.rs
+++ src/database.rs
@@ -374,7 +374,11 @@ pub trait DatabaseTransaction: Database {
     }
 }
 
+/// Provides transaction database operations
 pub trait DatabaseTransactionOps: Database + DatabaseTransactionIO {
+    /// Reads all [`DatabaseRecord`] with the given path from the stored snapshot in the transaction
+    ///
+    /// See [`DatabaseTransactionIO::try_read_storage_before`] for details and the list of possible errors.
     fn get_all_before_with_path<T: DatabaseRecord>(
         &self,
         transaction_path: impl AsRef<Path>,
@@ -382,12 +386,23 @@ pub trait DatabaseTransactionOps: Database + DatabaseTransactionIO {
         return self.try_read_storage_before::<Vec<T>>(transaction_path);
     }
 
+    /// Reads all [`DatabaseRecord`] from the given path from the stored snapshot in the transaction
+    ///
+    /// See [`DatabaseTransactionOps::get_all_before_with_path`] for details and the list of possible errors.
     fn get_all_before<T: DatabaseRecordPartitioned>(&self) -> Result<Vec<T>> {
         self.get_all_before_with_path::<T>(self.file_path(T::PARTITION))
     }
 }
 
+/// Provides I/O operations for transaction database
 pub trait DatabaseTransactionIO: Database {
+    /// Attempts to read data from the stored snapshot in the transaction and deserialize it into the 
+    /// specified type of data (usually some form of [`DatabaseRecord`])
+    ///
+    /// # Errors
+    /// - I/O
+    /// - Invalid path
+    /// - Parsing failure
     fn try_read_storage_before<O: for<'a> Deserialize<'a>>(
         &self,
         transaction_path: impl AsRef<Path>,
